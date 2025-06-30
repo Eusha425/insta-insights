c7abb899fd63fe321f7bb88e278bafcf1015c3bb
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2025-06-30 19:49:16 +1000

    Add file upload UI for Instagram follower insights
    Introduces a Streamlit interface with title, description, and file uploaders for followers and following JSON files. Adds validation to ensure only JSON files are accepted.

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.write("Hello world")
+#st.write("Hello world")
+
+# section for title and description phase 1
+st.title("📊 Instagram Follower Insights")
+st.write("Description here....")
+
+# upload the files
+upload_file_followers = st.file_uploader("Upload the followers file: ")
+#print(upload_file_followers)
+if upload_file_followers:
+    if '.json' not in upload_file_followers.name:
+        #st.write("invalid file type")
+        st.error("Please upload a JSON file")
+
+upload_file_following = st.file_uploader("Upload the following file: ")
+if upload_file_following:
+    if '.json' not in upload_file_following:
+        st.error("Please upload a JSON file")
