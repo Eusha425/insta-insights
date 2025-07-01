ebbccfca3e5a83b49bec32bcc32be42d94034e6e
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2025-07-02 00:06:04 +1000

    Add analysis button and state tracking for uploads
    Introduced flags to track follower and following file uploads, and added an 'Analyse data' button to trigger analysis when both files are uploaded. Prepared integration with the analyser module for future data analysis.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import data_loader as dl
+import analyser as anl
 #st.write("Hello world")
+FOLLOWING_UPLOADED = False
+FOLLOWER_UPLOADED = False
 
 # section for title and description phase 1
 st.title("📊 Instagram Follower Insights")
@@ -20,6 +23,7 @@ if upload_file_followers:
     else:
         followers = dl.find_followers(upload_file_followers)
         st.write(followers)
+        FOLLOWER_UPLOADED = True
 
 upload_file_following = st.file_uploader("Upload the following file: ")
 if upload_file_following:
@@ -29,3 +33,25 @@ if upload_file_following:
     else:
         following = dl.find_following(upload_file_following)
         st.write(following)
+        FOLLOWING_UPLOADED = True
+
+# if upload_file_followers and upload_file_following:
+#     if '.json' not in upload_file_followers.name:
+#         st.error("Please upload a JSON file")
+#     elif '.json' not in upload_file_following.name:
+#         st.error("Please upload a JSON file")
+#     else:
+#         followers = dl.find_followers(upload_file_followers)
+#         st.write(followers)
+#         following = dl.find_following(upload_file_following)
+#         st.write(following)
+
+#         non_followers, unrequited_followers, mutual = anl.following_follower_analysis(following, followers)
+
+analyse_button = st.button("Analyse data")
+
+if analyse_button:
+    if FOLLOWER_UPLOADED == True and FOLLOWING_UPLOADED == True:
+        non_followers, unrequited_followers, mutual = anl.following_follower_analysis(following, followers)
+        st.write("in condition")
+
